76878699e895aa971c1390f4b953e9ec71632ac8
talesCPV <tales@flexibus.com.br>
2023-01-20 22:26:29 -0300

    by_script(home) -> sex 20 jan 2023 22:26:29 -03

--- backend/setLog.php
+++ backend/setLog.php
@@ -2,11 +2,13 @@
 
 	if (IsSet($_POST["line"])){
         $path = getcwd().'/../config/log/'.date("m_Y").'.txt';
-        $line = $_POST["line"];    
+//        $line = $_POST["line"]; 
+        $line = mb_convert_encoding($_POST["line"],'UTF-8'); 
+        
         $fp = fopen($path, "a+");
 //        $fp = "\xEF\xBB\xBF".$fp;
 //        fputs($fp, $line."\n");
-        fwrite($fp, utf8_encode($line)."\n"); 
+        fwrite($fp, utf8_encode($line."\n")); 
         fclose($fp);    
     }
 
